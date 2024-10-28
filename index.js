1ac4966b972beb466a56453398f6c1d2a547e101
Akash <akashyoungstar@gmail.com>
2024-10-28 20:34:48 +0530

    url update

--- index.js
+++ index.js
@@ -10,16 +10,21 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://inventory-frontend-kappa-liart.vercel.app"], // Allow requests from your Vite dev server
+    origin: [
+      "http://localhost:5173",
+      "https://inventory-frontend-kappa-liart.vercel.app",
+      "https://inventory-frontend-4kqb7knr4-akashs-projects-848d32a6.vercel.app",
+    ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // If you're sending cookies or authorization headers
   })
 );
+
 app.use(express.json());
 app.get("/", (req, res) => res.send("hello my friend"));
 app.use("/api", authRoute);
 
 app.listen(3000, () => {
-    mongoDB();
+  mongoDB();
   console.log("server is running ...");
 });
